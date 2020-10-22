4d9f60c63e8bc12eb6c7a42a5447a8b13b25392c
Wasawsky <mjbc_20@hotmail.com>
2020-10-22 14:42:09 -0500

    mapeo

--- src/main/java/edu/eci/arsw/parcial/services/http/HttpConnectionExample.java
+++ src/main/java/edu/eci/arsw/parcial/services/http/HttpConnectionExample.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HttpConnectionExample {
@@ -12,6 +13,37 @@ public class HttpConnectionExample {
     private static final String USER_AGENT = "Mozilla/5.0";
     private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
 
+    public static String getRequest(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        String json = null;
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println(response.toString());
+            json = response.toString();
+        } else {
+            System.out.println("GET request not worked");
+        }
+        System.out.println("GET DONE");
+        return json;
+    }
     public static void main(String[] args) throws IOException {
 
         URL obj = new URL(GET_URL);
--- src/main/java/edu/eci/arsw/parcial/services/impl/WeaterServicesImpl.java
+++ src/main/java/edu/eci/arsw/parcial/services/impl/WeaterServicesImpl.java
@@ -1,16 +1,28 @@
 package edu.eci.arsw.parcial.services.impl;
 
+import edu.eci.arsw.parcial.controllers.ResourceNotFoundException;
 import edu.eci.arsw.parcial.model.Weather;
 import edu.eci.arsw.parcial.services.WeatherServices;
+import edu.eci.arsw.parcial.services.http.HttpConnectionExample;
 
-public class WeaterServicesImpl implements WeatherServices{
+public class WeaterServicesImpl implements WeatherServices {
 
     @Override
     public Weather getWeatherCity(String city) {
         // TODO Auto-generated method stub
-        Weather wht =null;
+        Weather wht = null;
         System.out.println("gettando");
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=eef1796447aa9216af254cb36e39c2af";
+        try {
+            HttpConnectionExample.getRequest(url);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
         return wht;
     }
-    
+
+    public static void main(String[] args) throws ResourceNotFoundException {
+        WeatherServices wht = new WeaterServicesImpl();
+        wht.getWeatherCity("london");
+    }
 }
