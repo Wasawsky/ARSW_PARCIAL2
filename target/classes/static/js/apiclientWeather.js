f6934b54e5fff3bdb7106b61108efc89ad132c95
Wasawsky <mjbc_20@hotmail.com>
2020-10-22 13:32:05 -0500

    servicios

--- target/classes/static/js/apiclientWeather.js
+++ target/classes/static/js/apiclientWeather.js
@@ -1,35 +1,12 @@
-apiclient = (function () {
-
-
+apiclientWeather = (function () {
 
     return {
-        getFunctionsByCinema: function (cinema_name, callback) {
-            $.getJSON('/cinemas/' + cinema_name, function (response) {
+        getWeatherCity: function (city, callback) {
+            $.getJSON('/weather/' + city, function (response) {
                 callback(response);
             }).fail(function () {
                 callback(undefined);
             })
-        },
-        getFunctionsByCinemaAndDate: function (cinema_name, fdate, callback) {
-            if (fdate === "") {
-                $.getJSON('/cinemas/' + cinema_name, function (response) {
-                    callback(response['functions']);
-                }).fail(function () {
-                    callback(undefined);
-                })
-            } else {
-                $.getJSON('/cinemas/' + cinema_name + '/' + fdate, function (response) {
-                    console.log("HPT");
-                    callback(response);
-                }).fail(function () {
-                    callback(undefined);
-                })
-            }
-
-        },
-
-        buyTicket: function (row, col) {
-            //buy
         }
     }
 })();
